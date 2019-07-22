057510cb59fb2f9e76ccfed8fac71a6a46eea55b
Julianne Swinoga <julianneswinoga@gmail.com>
2019-07-22 16:01:38 -0400

    Basic scaling and updating

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,7 +9,6 @@ use ggez::graphics;
 use ggez::timer;
 use ggez::{Context, GameResult};
 use num_complex::Complex;
-use num_traits::pow::Pow;
 
 use pixeling::*;
 
@@ -57,51 +56,64 @@ fn pix_to_cmplx(
     Complex::new(re, im)
 }
 
-struct MainState {
+fn update_mandel(
+    ctx: &mut Context,
     top_left_scale: Complex<FloatPrecision>,
     bottom_right_scale: Complex<FloatPrecision>,
+) -> graphics::Image {
+    let mut pix_img = PixelImage::new(WIN_WIDTH, WIN_HEIGHT);
+
+    for i in 0..pix_img.width {
+        for j in 0..pix_img.height {
+            let point0 = pix_to_cmplx(top_left_scale, bottom_right_scale, i, j);
+            let mut point = Complex::<FloatPrecision>::new(0.0, 0.0);
+
+            let mut iteration: usize = 0;
+            while (point * point).re <= 2.0 * 2.0 && iteration < MAX_ITER {
+                let re_temp = point.re * point.re - point.im * point.im + point0.re;
+                point.im = 2.0 * point.re * point.im + point0.im;
+                point.re = re_temp;
+
+                iteration += 1;
+            }
+
+            let pix = RgbaPixel::from_rainbow(iteration);
+
+            pix_img.pixels[j][i].set(pix.r, pix.g, pix.b, pix.a);
+        }
+    }
+
+    let flattened: Vec<u8> = pix_img.flat();
+    let image =
+        graphics::Image::from_rgba8(ctx, pix_img.width as u16, pix_img.height as u16, &flattened)
+            .unwrap();
+
+    image
+}
+
+struct MainState {
+    top_left_scale: Complex<FloatPrecision>,
+    last_top_left_scale: Complex<FloatPrecision>,
+    bottom_right_scale: Complex<FloatPrecision>,
+    last_bottom_right_scale: Complex<FloatPrecision>,
     image: graphics::Image,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let mut pix_img = PixelImage::new(WIN_WIDTH, WIN_HEIGHT);
-        let top_left_scale: Complex<FloatPrecision> = Complex::new(-2.5, -1.0);
-        let bottom_right_scale: Complex<FloatPrecision> = Complex::new(1.0, 1.0);
+        let default_image =
+            graphics::Image::solid(ctx, WIN_WIDTH as u16, ggez::graphics::BLACK).unwrap();
 
-        for i in 0..pix_img.width {
-            for j in 0..pix_img.height {
-                let point0 = pix_to_cmplx(top_left_scale, bottom_right_scale, i, j);
-                let mut point = Complex::<FloatPrecision>::new(0.0, 0.0);
-
-                let mut iteration: usize = 0;
-                while (point * point).re <= 2.0 * 2.0 && iteration < MAX_ITER {
-                    let re_temp = point.re * point.re - point.im * point.im + point0.re;
-                    point.im = 2.0 * point.re * point.im + point0.im;
-                    point.re = re_temp;
-
-                    iteration += 1;
-                }
-
-                let pix = RgbaPixel::from_rainbow(iteration);
-
-                pix_img.pixels[j][i].set(pix.r, pix.g, pix.b, pix.a);
-            }
-        }
-
-        let flattened: Vec<u8> = pix_img.flat();
-        let image = graphics::Image::from_rgba8(
-            ctx,
-            pix_img.width as u16,
-            pix_img.height as u16,
-            &flattened,
-        )
-        .unwrap();
+        let default_top_left_scale = Complex::<FloatPrecision>::new(-2.5, -1.0);
+        let default_bottom_right_scale = Complex::<FloatPrecision>::new(1.0, 1.0);
 
         let s = MainState {
-            top_left_scale,
-            bottom_right_scale,
-            image,
+            top_left_scale: default_top_left_scale,
+            last_top_left_scale: Complex::<FloatPrecision>::new(0.0, 0.0),
+            bottom_right_scale: default_bottom_right_scale,
+            last_bottom_right_scale: Complex::<FloatPrecision>::new(0.0, 0.0),
+
+            image: default_image,
         };
 
         Ok(s)
@@ -111,7 +123,17 @@ impl MainState {
 impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         const DESIRED_FPS: u32 = 30;
-        while timer::check_update_time(ctx, DESIRED_FPS) {}
+        while timer::check_update_time(ctx, DESIRED_FPS) {
+            if self.top_left_scale != self.last_top_left_scale
+                || self.bottom_right_scale != self.last_bottom_right_scale
+            {
+                println!("Updating");
+                self.image = update_mandel(ctx, self.top_left_scale, self.bottom_right_scale);
+                self.last_top_left_scale = self.top_left_scale;
+                self.last_bottom_right_scale = self.bottom_right_scale;
+                println!("Done");
+            }
+        }
         Ok(())
     }
 
@@ -128,6 +150,18 @@ impl event::EventHandler for MainState {
 
         timer::yield_now();
         Ok(())
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        _btn: event::MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        println!("Click at {} {}", x, y);
+        self.top_left_scale = self.top_left_scale / 2.0;
+        self.bottom_right_scale = self.bottom_right_scale / 2.0;
     }
 }
 
